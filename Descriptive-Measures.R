29ee62b77e9f297c41e762419e66fc53c5de364c
zstephens21 <zstephens@claflin.edu>
2026-08-18 16:13:05 -0400

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,21 @@
+#Name: Zora Stephens, Date:8/18/26, Purpose: To test functions of descriptive statistics
+
+#Create a variable called variable 1 and store 5 numbers form 1-5
+
+variable1 <- c(1,2,3,4,5)
+
+#Calculate mean of variable1
+
+mean(variable1)
+[1] 3
+
+#Calculate standard deviation
+
+sd(variable1)
+[1] 1.581139
+
+#Calulate median
+
+median(variable1)
+[1] 3
+
